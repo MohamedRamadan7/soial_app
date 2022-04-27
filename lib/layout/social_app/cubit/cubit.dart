cd35862819cf077b22584b946def0e2e1598ae8e
Mohamed Ramadan <mohamedramadan6420@gmail.com>
2022-04-27 17:18:44 +0200

    Upload

---
+++ lib/layout/social_app/cubit/cubit.dart
@@ -0,0 +1,55 @@
+import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:untitled/layout/social_app/cubit/states.dart';
+import 'package:untitled/models/social_app.dart';
+import 'package:untitled/modules/social_app/chats/chats_screen.dart';
+import 'package:untitled/modules/social_app/feeds/feeds_screen.dart';
+import 'package:untitled/modules/social_app/new_post/new_post_screen.dart';
+import 'package:untitled/modules/social_app/notificatians/notifications_screen.dart';
+import 'package:untitled/modules/social_app/settings/settings_screen.dart';
+import 'package:untitled/modules/social_app/users/users_screen.dart';
+import 'package:untitled/shared/components/constants.dart';
+
+class SocialCubit extends Cubit<SocialStates> {
+  SocialCubit() : super(SocialInitialState());
+  static SocialCubit get(context) => BlocProvider.of(context);
+  SocialUserModel? model;
+  void getUserData() {
+    emit(SocialGetUserLoadingState());
+    FirebaseFirestore.instance.collection('users').doc(uId).get().then((value) {
+      model = SocialUserModel.fromJson(value.data()!);
+      print('data is :$model');
+      emit(SocialGetUserSuccessState());
+    }).catchError((error) {
+      print(error.toString());
+      emit(SocialGetUserErrorState(error.toString()));
+    });
+  }
+
+  int currentIndex = 0;
+  List<Widget> screens = [
+    FeedsScreen(),
+    ChatsScreen(),
+    NewPostScreen(),
+    UsersScreen(),
+    SettingsScreen(),
+  ];
+
+  List<String> titles = [
+    'Home',
+    'chats',
+    'Add Post',
+    'Users',
+    'settings',
+  ];
+
+  void changeBottomNav(int index) {
+    if (index == 2) {
+      emit(SocialNewPostState());
+    } else
+      currentIndex = index;
+    emit(SocialChangeBottomNavState());
+  }
+}
